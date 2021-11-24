ba3c7d4cfe65b98153653b7314c4f221c19c224f
matthewhirai <mhirai301@gmail.com>
2021-11-23 18:07:51 -0800

    added Grid component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import HeroImage from "./HeroImage";
+import Grid from "./Grid";
 import NoImage from '../images/no_image.jpg'
 import { useHomeFetch } from '../hooks/useHomeFetch'
 
@@ -16,6 +17,11 @@ const Home = () => {
                     title={state.results[0].original_title} 
                     text={state.results[0].overview}/>) 
                 : null}
+            <Grid header="Popular Movies">
+                {state.results.map(movie => (
+                    <div key={movie.id}>{movie.title}</div>
+                ))}
+            </Grid>
         </>
     )
 };
