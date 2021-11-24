f483374a3fff3670c649f3ce316650f188a9bc4e
matthewhirai <mhirai301@gmail.com>
2021-11-23 22:18:12 -0800

    added MovieInfoBar component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -5,6 +5,7 @@ import Grid from "./Grid";
 import Spinner from "./Spinner";
 import BreadCrumb from "./BreadCrumb";
 import MovieInfo from "./MovieInfo";
+import MovieInfoBar from "./MovieInfoBar";
 import { useMovieFetch } from "../hooks/useMovieFetch";
 import NoImage from '../images/no_image.jpg'
 
@@ -19,6 +20,7 @@ const Movie = () => {
         <>
             <BreadCrumb movieTitle={movie.original_title} />
             <MovieInfo movie={movie} />
+            <MovieInfoBar time={movie.runtime} budget={movie.budget} revenue={movie.revenue} />
         </>
     )
 };
