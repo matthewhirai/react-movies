8700f117e7d7e02881792e9e29fecf72608ae61d
matthewhirai <mhirai301@gmail.com>
2021-11-23 17:04:52 -0800

    added props to HeroImage

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
+import HeroImage from "./HeroImage";
 import NoImage from '../images/no_image.jpg'
 import { useHomeFetch } from '../hooks/useHomeFetch'
 
@@ -8,7 +9,14 @@ const Home = () => {
     console.log(state);
 
     return (
-        <div>Home Page</div>
+        <>  
+            {state.results[0] 
+                ? (<HeroImage 
+                    image={`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`} 
+                    title={state.results[0].original_title} 
+                    text={state.results[0].overview}/>) 
+                : null}
+        </>
     )
 };
 
