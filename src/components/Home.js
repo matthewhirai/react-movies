9085f453e3f1acd52076f56f88fb295e863b718b
matthewhirai <mhirai301@gmail.com>
2021-11-23 18:57:13 -0800

    added SearchBar and setSearchTerm hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -4,11 +4,12 @@ import HeroImage from "./HeroImage";
 import Grid from "./Grid";
 import Thumb from "./Thumb";
 import Spinner from "./Spinner";
+import SearchBar from "./SearchBar";
 import NoImage from '../images/no_image.jpg'
 import { useHomeFetch } from '../hooks/useHomeFetch'
 
 const Home = () => {
-    const { state, loading, error } = useHomeFetch();
+    const { state, loading, error, setSearchTerm } = useHomeFetch();
     console.log(state);
 
     return (
@@ -19,6 +20,7 @@ const Home = () => {
                     title={state.results[0].original_title} 
                     text={state.results[0].overview}/>) 
                 : null}
+            <SearchBar setSearchTerm={setSearchTerm} />
             <Grid header="Popular Movies">
                 {state.results.map(movie => (
                     <Thumb 
