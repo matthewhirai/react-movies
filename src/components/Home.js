07d0901d9d05e01e794b6f14520948c2f9e0d4da
matthewhirai <mhirai301@gmail.com>
2021-11-23 18:18:27 -0800

    added Thumb component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL } from '../config';
 import HeroImage from "./HeroImage";
 import Grid from "./Grid";
+import Thumb from "./Thumb";
 import NoImage from '../images/no_image.jpg'
 import { useHomeFetch } from '../hooks/useHomeFetch'
 
@@ -19,7 +20,16 @@ const Home = () => {
                 : null}
             <Grid header="Popular Movies">
                 {state.results.map(movie => (
-                    <div key={movie.id}>{movie.title}</div>
+                    <Thumb 
+                        key={movie.id}
+                        clickable
+                        image={
+                            movie.poster_path
+                                ? IMAGE_BASE_URL + POSTER_SIZE + movie.poster_path
+                                : NoImage
+                        }
+                        movieId={movie.id}
+                    />
                 ))}
             </Grid>
         </>
